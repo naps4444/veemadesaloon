ae5b28f05f2879687d92bf9d96e673d09b651720
naps4444 <uchemadunmerengwa@gmail.com>
2025-07-26 08:57:12 +0100

    modified footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
             <div className=' text-white flex flex-col md:flex-row gap-4 p-3 md:px-4 py-6 2xl:container mx-auto justify-between md:text-center font-croissant-one'>
             <div className='text-[14px]'>
                 <h1 className='text-[18px] '>SAY HELLO</h1>
-                <p className='mt-2'>Phone +234-903-668-2394</p>
-                <p>Email: info@mysite.com</p>
+                <p className='mt-2'>Phone: +234-903-668-2394</p>
+                <p>Email: veemadesaloon@gmail.com</p>
 
                 <div className='flex justify-between md:mx-auto mt-1 w-[80px]'>
                     <Image src="/fb.svg" alt='icon veemade facebook' width={20} height={20} className='' />
@@ -23,15 +23,15 @@ const Footer = () => {
 
             <div className='text-[14px] md:w-4/12'>
                 <h1 className='text-[18px]'>OUR ADDRESS</h1>
-                <p className='mt-2 '>400 Terry Francine Street Lekki Ajah Lagos</p>
+                <p className='mt-2 '>8b The Providence St, Lekki Phase I, Lekki 106104, Lagos</p>
 
             </div>
 
             <div className='text-[14px]'>
                 <h1 className='text-[18px]'>OPENING HOURS</h1>
-                <p className='mt-2'>Mon - Fri 7am - 10pm</p>
-                <p>Saturday 8am - 10pm</p>
-                <p>Sunday 8am - 11pm</p>
+                <p className='mt-2'>Mon - Fri 8am - 10pm</p>
+                <p>Saturday 9am - 10pm</p>
+                <p>Sunday 9am - 11pm</p>
             </div>
 
             </div>
